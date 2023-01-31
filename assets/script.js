5af10086402d9e52d14eae145b16210dc90b1c5d
Michael Matheus <michaelmatheus2001@gmail.com>
2023-01-31 08:00:23 -0300

    refactor: change to OO paradigm

--- assets/script.js
+++ assets/script.js
@@ -1,44 +1,13 @@
-const resultScreen = document.querySelector('.calculator__result'); // get result screen
-const allButtons = document.querySelectorAll('.calculator__button'); // get all buttons: NodeList
-const allButtonsArr = Array.from(allButtons); // convert buttons NodeList into an Array
-
-
-
-/**
- * Filter numeric buttons
- */
-function numberButtonsFilter(btn){
-    if(!btn.classList.contains("calculator__button--orange") && !btn.classList.contains("calculator__button--light-medium") && !btn.classList.contains("calculator__button--comma")){
-        return btn;
+class Calculator {
+    constructor(prevOper, nextOper){
+        this.value = prevOper + nextOper;
     }
 }
 
-const numberButtons = allButtonsArr.filter(numberButtonsFilter);
-
-const addNumber = numberButtons.forEach(function(numberButton){
-    numberButton.addEventListener('click', () => {
-        if(resultScreen.textContent){
-            resultScreen.innerHTML += numberButton.textContent;
-        }
-    })
-});
 
 
-
-/**
- * Clear Screen Value (Reset to 0)
- */
-function clearScreen(){
-    resultScreen.innerHTML = "";
-}
-
-const clearBtn = document.querySelector('.calculator__button--clear');
-
-clearBtn.addEventListener('click', clearScreen);
-
-clearScreen();
+const screen = document.querySelector('.calculator__result'); // get screen element
+const allButtons = document.querySelectorAll('.calculator__button'); // get all buttons: NodeList
 
 
-
-
-
+// const calculator = new Calculator(56, 89);
